dd09957af10b75962e460ebd47584c2f04b60bd9
Syazwi Zaili <syazwizaili@Macbooks-MacBook-Pro-2.local>
2023-03-26 00:05:40 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avite',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
